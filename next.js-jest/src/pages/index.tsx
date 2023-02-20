348cfbb1286e5346e1bcad9bf19aeb424646ed53
katayama8000 <tattu.0310@gmail.com>
2023-02-21 02:26:05 +0900

    commit

---
+++ next.js-jest/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+import { Flex } from '@chakra-ui/react'
+
+import MainHeading from '@/components/main-heading/main-heading'
+import { APP_NAME } from '@/config'
+
+const Home: NextPage = () => (
+  <Flex bg='tomato' minHeight='100vh' alignItems='center' justifyContent='center'>
+    <Head>
+      <meta charSet='utf-8' />
+      <title>{APP_NAME}</title>
+    </Head>
+    <MainHeading>Hello World.</MainHeading>
+  </Flex>
+)
+
+export default Home
